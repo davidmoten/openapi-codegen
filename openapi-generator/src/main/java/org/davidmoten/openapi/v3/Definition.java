b8455d3d0baad08338bfc14f2c911a5b144f384e
David Moten <davidmoten@gmail.com>
2020-12-16 19:28:34 +1100

    dev

--- openapi-generator/src/main/java/org/davidmoten/openapi/v3/Definition.java
+++ openapi-generator/src/main/java/org/davidmoten/openapi/v3/Definition.java
@@ -3,9 +3,13 @@ package org.davidmoten.openapi.v3;
 public final class Definition {
     
     private final String definition;
+    private final String modelPackage;
+    private final String clientPackage;
     
-    public Definition(String definition) {
+    public Definition(String definition, String modelPackage, String clientPackage) {
         this.definition = definition;
+        this.modelPackage = modelPackage;
+        this.clientPackage = clientPackage;
     }
     
     public String definition() {
--- openapi-generator/src/main/java/org/davidmoten/openapi/v3/Generator.java
+++ openapi-generator/src/main/java/org/davidmoten/openapi/v3/Generator.java
@@ -1,29 +1,26 @@
 package org.davidmoten.openapi.v3;
 
-import java.util.List;
-
 import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 
 public final class Generator {
 
-    private final List<Definition> definitions;
+    private final Definition definition;
 
-    public Generator(List<Definition> definitions) {
-        this.definitions = definitions;
+    public Generator(Definition definition) {
+        this.definition = definition;
     }
 
     public void generate() {
-        for (Definition definition : definitions) {
-            SwaggerParseResult result = new OpenAPIParser().readContents(definition.definition(), null, null);
-            result.getMessages().stream().forEach(System.out::println);
-            OpenAPI api = result.getOpenAPI();
-            System.out.println(api);
+        SwaggerParseResult result = new OpenAPIParser().readContents(definition.definition(), null, null);
+        result.getMessages().stream().forEach(System.out::println);
+        OpenAPI api = result.getOpenAPI();
+        System.out.println(api);
 
-            // generate methods on singleton client object
+        // generate methods on singleton client object in client package
+        // generate model classes for schema definitions 
 
-        }
     }
 
 }
