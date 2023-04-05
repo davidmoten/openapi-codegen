170940eb25278ba7b117045a548a17e8f6c6d75f
Dave Moten <davidmoten@gmail.com>
2023-04-06 05:28:01 +1000

    spring boot dev

--- openapi-codegen-example-pet-store/src/main/java/pet/store/Service.java
+++ openapi-codegen-example-pet-store/src/main/java/pet/store/Service.java
@@ -1,23 +1,20 @@
 package pet.store;
 
 import java.util.Collections;
-import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import pet.store.model.Error;
 import pet.store.model.Path_pets_Get_200_Response;
-import pet.store.model.Path_pets_Get_tags_Parameter_tags;
 
 @RestController
 public class Service {
 
     @RequestMapping(method = RequestMethod.GET, value = "/pets", produces = { "application/json" })
-    public ResponseEntity<?> pets(@Nullable Path_pets_Get_tags_Parameter_tags tags, @Nullable Integer limit) {
+    public ResponseEntity<?> pets() {
         try {
             return ResponseEntity.ofNullable(new Path_pets_Get_200_Response(Collections.emptyList()));
         } catch (RuntimeException e) {
