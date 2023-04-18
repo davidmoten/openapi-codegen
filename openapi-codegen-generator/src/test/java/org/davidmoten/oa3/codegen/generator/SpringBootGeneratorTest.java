231fe4cce903ba2b5ec02a6cabed14ce01b03b3e
Dave Moten <davidmoten@gmail.com>
2023-04-18 12:20:17 +1000

    minor dev

---
+++ openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/SpringBootGeneratorTest.java
@@ -0,0 +1,20 @@
+package org.davidmoten.oa3.codegen.generator;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class SpringBootGeneratorTest {
+    
+    @Test
+    public void test() throws MalformedURLException {
+        String definition = new File("../openapi-codegen-maven-plugin-test/src/main/openapi/openapi.yml").toURI().toURL().toExternalForm();
+        Packages packages = new Packages("test");
+        Definition d = new Definition(definition, packages, new File("target/generated-source/java"), x -> x,
+                Collections.emptySet(), Collections.emptySet(), false, true);
+        new SpringBootGenerator(new Names(d)).generate();
+    }
+
+}
