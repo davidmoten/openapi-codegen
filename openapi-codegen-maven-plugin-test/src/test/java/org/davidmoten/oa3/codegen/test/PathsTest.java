b587bc10bfc868bd2045af927b6aed21781b8568
Dave Moten <davidmoten@gmail.com>
2023-04-22 10:42:55 +1000

    add parameter annotation tests

--- openapi-codegen-maven-plugin-test/src/test/java/org/davidmoten/oa3/codegen/test/PathsTest.java
+++ openapi-codegen-maven-plugin-test/src/test/java/org/davidmoten/oa3/codegen/test/PathsTest.java
@@ -20,8 +20,10 @@ import org.davidmoten.oa3.codegen.spring.runtime.ServiceException;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @SuppressWarnings("unused")
 public class PathsTest {
@@ -32,7 +34,8 @@ public class PathsTest {
     public void testRequestBodyRequired() throws ServiceException {
         RequestBody1 r = new RequestBody1("fred");
         Response1 response = service.requestBodyRequiredPost(r);
-        hasParameterAnnotation(ServiceController.class, RequestBody.class, "requestBodyRequiredPost", 0, RequestBody1.class);
+        hasParameterAnnotation(ServiceController.class, RequestBody.class, "requestBodyRequiredPost", 0,
+                RequestBody1.class);
     }
 
     @Test
@@ -83,9 +86,14 @@ public class PathsTest {
     }
 
     @Test
-    public void testPathVariable() throws ServiceException {
-        service.paramsIdGet("abc");
-        hasParameterAnnotation(ServiceController.class, PathVariable.class, "paramsIdGet", 0, String.class);
+    public void testPathVariableAndRequestHeaderAndCookie() throws ServiceException {
+        service.paramsIdGet("abc", "apikey", "timtam");
+        hasParameterAnnotation(ServiceController.class, PathVariable.class, "paramsIdGet", 0, String.class,
+                String.class, String.class);
+        hasParameterAnnotation(ServiceController.class, RequestHeader.class, "paramsIdGet", 1, String.class,
+                String.class, String.class);
+        hasParameterAnnotation(ServiceController.class, CookieValue.class, "paramsIdGet", 2, String.class, String.class,
+                String.class);
     }
 
     private static void hasParameterAnnotation(Class<?> c, Class<? extends Annotation> annotation, String methodName,
