a5b2be2fed30c1471fb694f2876dfe5e32ac8c9a
Dave Moten <davidmoten@gmail.com>
2023-05-04 09:38:10 +1000

    use @SpringBootTest more and suppress test logging, add missing files

---
+++ openapi-codegen-maven-plugin-test/src/test/java/org/davidmoten/oa3/codegen/paths/PathsApplication.java
@@ -0,0 +1,15 @@
+package org.davidmoten.oa3.codegen.paths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties
+@SpringBootApplication
+public class PathsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathsApplication.class, args);
+    }
+
+}
