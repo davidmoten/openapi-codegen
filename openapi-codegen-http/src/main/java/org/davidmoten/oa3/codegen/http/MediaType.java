988eead704ca7a81ddb754de11fba46a3ac68857
Dave Moten <davidmoten@gmail.com>
2023-05-04 11:49:20 +1000

    add binary media types to MediaType class

--- openapi-codegen-http/src/main/java/org/davidmoten/oa3/codegen/http/MediaType.java
+++ openapi-codegen-http/src/main/java/org/davidmoten/oa3/codegen/http/MediaType.java
@@ -1,17 +1,33 @@
 package org.davidmoten.oa3.codegen.http;
 
+import java.util.Locale;
+import java.util.Set;
+
+import com.github.davidmoten.guavamini.Sets;
+
 public final class MediaType {
 
+    private static final Set<String> BINARY_MEDIA_TYPES = Sets.newHashSet( //
+            "application/octet-stream", //
+            "application/pdf", //
+            "image/jpeg", //
+            "image/gif", //
+            "image/png");
+
     public static boolean isJson(String mediaType) {
-        return mediaType.endsWith("/json") || mediaType.endsWith("+json");
+        return lc(mediaType).endsWith("/json") || lc(mediaType).endsWith("+json");
+    }
+
+    private static String lc(String s) {
+        return s.toLowerCase(Locale.ENGLISH);
     }
 
     public static boolean isText(String mediaType) {
-        return mediaType.startsWith("text/");
+        return lc(mediaType).startsWith("text/");
     }
 
     public static boolean isOctetStream(String mediaType) {
-        return mediaType.equals("application/octet-stream");
+        return BINARY_MEDIA_TYPES.contains(lc(mediaType));
     }
 
 }
