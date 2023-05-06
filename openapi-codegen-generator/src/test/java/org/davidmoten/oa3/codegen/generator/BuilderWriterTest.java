2bfe7686c8cee11d93766017deb5b74c6e74998b
Dave Moten <davidmoten@gmail.com>
2023-05-06 22:00:49 +1000

    BuilderWriter dev

--- openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/BuilderWriterTest.java
+++ openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/BuilderWriterTest.java
@@ -1,5 +1,7 @@
 package org.davidmoten.oa3.codegen.generator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,37 +14,37 @@ import org.junit.jupiter.api.Test;
 public class BuilderWriterTest {
 
     @Test
-    public void testMixed() {
+    public void testMixed() throws FileNotFoundException {
         List<Field> fields = new ArrayList<>();
         fields.add(new Field("firstName", "String", true));
         fields.add(new Field("lastName", "String", true));
         fields.add(new Field("mobile", "String", false));
         fields.add(new Field("age", "Integer", false));
-        try (PrintWriter writer = new PrintWriter(System.out)) {
+        try (PrintWriter writer = new PrintWriter(new File("target/Mixed.java"))) {
             BuilderWriter.write(writer, new Indent().right(), fields, "Thing", Optional.of("Optional"));
         }
     }
 
     @Test
-    public void testJustOptional() {
+    public void testJustOptional() throws FileNotFoundException {
         List<Field> fields = new ArrayList<>();
         fields.add(new Field("firstName", "String", false));
         fields.add(new Field("lastName", "String", false));
         fields.add(new Field("mobile", "String", false));
         fields.add(new Field("age", "Integer", false));
-        try (PrintWriter writer = new PrintWriter(System.out)) {
+        try (PrintWriter writer = new PrintWriter(new File("target/Optionals.java"))) {
             BuilderWriter.write(writer, new Indent().right(), fields, "Thing", Optional.of("Optional"));
         }
     }
 
     @Test
-    public void testJustRequired() {
+    public void testJustRequired() throws FileNotFoundException {
         List<Field> fields = new ArrayList<>();
         fields.add(new Field("firstName", "String", true));
         fields.add(new Field("lastName", "String", true));
         fields.add(new Field("mobile", "String", true));
         fields.add(new Field("age", "Integer", true));
-        try (PrintWriter writer = new PrintWriter(System.out)) {
+        try (PrintWriter writer = new PrintWriter(new File("target/Required.java"))) {
             BuilderWriter.write(writer, new Indent().right(), fields, "Thing", Optional.of("Optional"));
         }
     }
