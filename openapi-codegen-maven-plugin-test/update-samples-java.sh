5bb9e4cae4b0f5390e30d83e7907dcae682cc62b
Dave Moten <davidmoten@gmail.com>
2023-05-10 21:10:33 +1000

    fix docs

--- openapi-codegen-maven-plugin-test/update-samples-java.sh
+++ openapi-codegen-maven-plugin-test/update-samples-java.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-mkdir -p ../src/main/docs
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/main/docs/Car.java 
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Bike.java ../src/main/docs/Bike.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Vehicle.java ../src/main/docs/Vehicle.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Geometry.java ../src/main/docs/Geometry.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Circle.java ../src/main/docs/Circle.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Rectangle.java ../src/main/docs/Rectangle.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/main/docs/Car.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/main/docs/Car.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/main/docs/Car.java
-cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/main/docs/Car.java
+mkdir -p ../src/docs
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/docs/Car.java 
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Bike.java ../src/docs/Bike.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Vehicle.java ../src/docs/Vehicle.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Geometry.java ../src/docs/Geometry.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Circle.java ../src/docs/Circle.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Rectangle.java ../src/docs/Rectangle.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/docs/Car.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/docs/Car.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/docs/Car.java
+cp target/generated-sources/java/org/davidmoten/oa3/codegen/test/main/schema/Car.java ../src/docs/Car.java
 
