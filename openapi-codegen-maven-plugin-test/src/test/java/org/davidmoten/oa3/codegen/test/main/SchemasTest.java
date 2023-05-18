c062d10a199cabb1fe99b385554b6a8c2151b118
Dave Moten <davidmoten@gmail.com>
2023-05-18 16:40:55 +1000

    msi update

--- openapi-codegen-maven-plugin-test/src/test/java/org/davidmoten/oa3/codegen/test/main/SchemasTest.java
+++ openapi-codegen-maven-plugin-test/src/test/java/org/davidmoten/oa3/codegen/test/main/SchemasTest.java
@@ -60,6 +60,7 @@ import org.davidmoten.oa3.codegen.test.main.schema.MinMaxLength;
 import org.davidmoten.oa3.codegen.test.main.schema.Msi;
 import org.davidmoten.oa3.codegen.test.main.schema.MsiId;
 import org.davidmoten.oa3.codegen.test.main.schema.NamesWithSpaces;
+import org.davidmoten.oa3.codegen.test.main.schema.NonSARPriority;
 import org.davidmoten.oa3.codegen.test.main.schema.ObjectAllOptionalFields;
 import org.davidmoten.oa3.codegen.test.main.schema.ObjectNoOptionalFields;
 import org.davidmoten.oa3.codegen.test.main.schema.Oval3;
@@ -729,18 +730,11 @@ public class SchemasTest {
 
     @Test
     public void testMsi() throws JsonProcessingException {
-        String json = "{\"id\":\"8ds9f8sd98-dsfds8989\",\"broadcast\":{\"area\":{\"lat\":25.1,\"lon\":-33.1,\"radiusNm\":1.0}},\"createdTime\":\"2023-04-05T12:15:26.025+10:00\",\"startTime\":\"2023-04-05T14:15:26.025+10:00\",\"endTime\":\"2023-04-06T12:00:26.025+10:00\",\"status\":\"ACTIVE\"}\n";
-        {
-            Msi a = m.readValue(json, Msi.class);
-            MetBroadcast b = (MetBroadcast) a.broadcast().value();
-            Geometry g = (Geometry) b.area().value();
-            Circle c = (Circle) g.value();
-            assertEquals(25.1, c.lat().value(), 0.0001);
-        }
+        String json = "{\"id\":\"8ds9f8sd98-dsfds8989\",\"broadcast\":{\"area\":{\"lat\":25.1,\"lon\":-33.1,\"radiusNm\":1.0},\"priority\":\"SAFETY\"},\"createdTime\":\"2023-04-05T12:15:26.025+10:00\",\"startTime\":\"2023-04-05T14:15:26.025+10:00\",\"endTime\":\"2023-04-06T12:00:26.025+10:00\",\"status\":\"ACTIVE\"}\n";
         {
             Circle circle = new Circle(new Latitude(25.1F), new Longitude(-33.1F), 1);
             MetBroadcastArea mbca = MetBroadcastArea.of(Geometry.of(circle));
-            MetBroadcast mbc = MetBroadcast.builder().area(mbca).build();
+            MetBroadcast mbc = MetBroadcast.builder().area(mbca).priority(NonSARPriority.SAFETY).build();
             Broadcast broadcast = Broadcast.of(mbc);
             OffsetDateTime createdTime = OffsetDateTime.parse("2023-04-05T12:15:26.025+10:00");
             OffsetDateTime startTime = OffsetDateTime.parse("2023-04-05T14:15:26.025+10:00");
@@ -756,6 +750,14 @@ public class SchemasTest {
                     .build();
             assertEquals(m.readTree(json), m.readTree(m.writeValueAsString(msi)));
         }
+        {
+            Msi a = m.readValue(json, Msi.class);
+            MetBroadcast b = (MetBroadcast) a.broadcast().value();
+            Geometry g = (Geometry) b.area().value();
+            Circle c = (Circle) g.value();
+            assertEquals(25.1, c.lat().value(), 0.0001);
+        }
+
     }
 
     @Test
