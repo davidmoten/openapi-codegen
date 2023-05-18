82c584c322e4507dd57bfbeb75e302192b59e9b9
Dave Moten <davidmoten@gmail.com>
2023-05-18 13:36:32 +1000

    Revert "dev"
    This reverts commit e7bd325b8716f8b54fb14a2ce44bb47f24e6e68f.

--- openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/GeneratorTest.java
+++ openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/GeneratorTest.java
@@ -31,11 +31,6 @@ public final class GeneratorTest {
         generate("openflow.yml");
     }
 
-    @Test
-    public void testGenerateSmall() throws IOException {
-        generate("small.yml");
-    }
-    
     private static void generate(String name) throws MalformedURLException {
         String definition = new File("../openapi-codegen-maven-plugin-test/src/main/openapi/" + name).toURI().toURL()
                 .toExternalForm();
