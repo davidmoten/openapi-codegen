3be3479d18e25f6141363b54c9b7fe72b7b1b14e
Dave Moten <davidmoten@gmail.com>
2023-05-21 18:53:49 +1000

    escape slash characters in field generated javadoc

--- openapi-codegen-generator/src/main/java/org/davidmoten/oa3/codegen/generator/writer/ServerCodeWriterSpringBoot.java
+++ openapi-codegen-generator/src/main/java/org/davidmoten/oa3/codegen/generator/writer/ServerCodeWriterSpringBoot.java
@@ -246,7 +246,7 @@ public final class ServerCodeWriterSpringBoot {
         Map<String, String> parameterDescriptions = m.parameters //
                 .stream() //
                 .collect(Collectors.toMap(x -> x.identifier,
-                        x -> x.description.orElse(x.identifier).replaceAll("\\n\\s*", " ")));
+                        x -> x.description.orElse(x.identifier).replaceAll("\\n\\s*", " ").replaceAll("/", "&#47;")));
 
         String html = m.description.map(x -> WriterUtil.markdownToHtml(x))
                 .orElse("<p>Returns response from call to path <i>%s</i>.</p>");
