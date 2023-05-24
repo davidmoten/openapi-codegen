30f7fcdd4647b2d29696299abd422b9eee083734
Dave Moten <davidmoten@gmail.com>
2023-05-24 13:18:51 +1000

    use batch mode in test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 MAVEN_OPTS="-Xmx3g"
-mvn clean install -pl *test -P extras
-mvn clean install -pl *test -P bitbucket
-mvn clean install -pl *test -P mailchimp
-mvn clean install -pl *test -P github
-mvn clean install -pl *test -P docusign
+mvn clean install -B -P extras
+mvn clean install -B -pl *test -P bitbucket
+mvn clean install -B -pl *test -P mailchimp
+mvn clean install -B -pl *test -P github
+mvn clean install -B -pl *test -P docusign
