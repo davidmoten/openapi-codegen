afeaaea91b83fffecf41a853abcccdc5f2a00272
Dave Moten <davidmoten@gmail.com>
2023-05-24 12:25:55 +1000

    update test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 MAVEN_OPTS="-Xmx3g"
 mvn clean install -pl *test -P extras
-mvn clean install -pl *test -P github
 mvn clean install -pl *test -P bitbucket
-mvn clean install -pl *test -P docusign
 mvn clean install -pl *test -P mailchimp
+mvn clean install -pl *test -P github
+mvn clean install -pl *test -P docusign
