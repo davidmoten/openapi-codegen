ed2cf4c5ca721d86406b1d42cd3341b343953547
Dave Moten <davidmoten@gmail.com>
2023-09-20 20:46:06 +1000

    refactor

--- openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/SerializationTest.java
+++ openapi-codegen-generator/src/test/java/org/davidmoten/oa3/codegen/generator/SerializationTest.java
@@ -6,17 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
+import org.davidmoten.oa3.codegen.runtime.AnyOfSerializer;
 import org.davidmoten.oa3.codegen.runtime.Config;
 import org.davidmoten.oa3.codegen.runtime.NullEnumDeserializer;
 import org.davidmoten.oa3.codegen.runtime.PolymorphicDeserializer;
@@ -40,18 +38,13 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.github.davidmoten.guavamini.Preconditions;
 import com.google.common.collect.Lists;
 
@@ -572,96 +565,7 @@ public class SerializationTest {
             }
         }
     }
-
-    @SuppressWarnings("serial")
-    private static class AnyOfSerializer<T> extends StdSerializer<T> {
-
-        private final Class<T> cls;
-        private final ObjectMapper mapper;
-
-        protected AnyOfSerializer(Config config, Class<T> cls, Class<?>... classes) {
-            super(cls);
-            this.cls = cls;
-            this.mapper = config.mapper();
-        }
-
-        @Override
-        public void serialize(T value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            // loop through fields
-            List<Optional<?>> values = Arrays.stream(cls.getFields()) //
-                    .map(f -> {
-                        try {
-                            return (Optional<?>) f.get(value);
-                        } catch (IllegalArgumentException | IllegalAccessException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }) //
-                    .collect(Collectors.toList());
-            JsonNode node = values.stream() //
-                    .filter(Optional::isPresent) //
-                    .map(x -> {
-                        try {
-                            return mapper.writeValueAsString(x);
-                        } catch (JsonProcessingException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }) //
-                    .map(x -> {
-                        try {
-                            return mapper.readTree(x);
-                        } catch (JsonProcessingException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }) //
-                    .collect(Collectors.reducing((a, b) -> merge(a, b))).get();
-            gen.writeTree(node);
-        }
-    }
-
-    /**
-     * Merges b into a. Only works properly with anyOf serialization
-     * 
-     * @param a input that will be mutated to contain merged result
-     * @param b node to merge into a
-     */
-    private static JsonNode merge(JsonNode a, JsonNode b) {
-        if (a == null || b == null || !a.isArray() && !a.isObject()) {
-            return a;
-        }
-        if (a.isArray()) {
-            if (!b.isArray()) {
-                // shouldn't happen because identical paths are supposed to have the
-                // same values (but possibly objects that are extended with other properties)
-                throw new IllegalStateException("unexpected");
-            }
-            ArrayNode x = (ArrayNode) a;
-            ArrayNode y = (ArrayNode) b;
-            if (x.size() != y.size()) {
-                // shouldn't happen
-                throw new IllegalStateException("array lengths don't match");
-            }
-            for (int i = 0; i < x.size(); i++) {
-                merge(x.get(i), y.get(i));
-            }
-        }
-        if (a.isObject() && b.isObject()) {
-            Iterator<String> fieldNames = b.fieldNames();
-            while (fieldNames.hasNext()) {
-                String fieldName = fieldNames.next();
-                JsonNode node = a.get(fieldName);
-                // if field exists and is an embedded objects
-                if (node != null) {
-                    merge(node, b.get(fieldName));
-                } else {
-                    // Overwrite field
-                    JsonNode value = b.get(fieldName);
-                    ((ObjectNode) a).replace(fieldName, value);
-                }
-            }
-        }
-        return a;
-    }
-
+    
     public static final class SimpleName {
         @JsonValue
         public final String name;
