94ebc784eadd051d86196f59ddaa5de084f5aeb2
Dave Moten <davidmoten@gmail.com>
2023-10-04 21:17:34 +1100

    scripts dev

--- openapi-codegen-generator/analyse.sh
+++ openapi-codegen-generator/analyse.sh
@@ -3,7 +3,5 @@ cd ../../openapi-directory
 curr=`pwd`
 find . -name "openapi.yaml"|sort|while read f; do
   cd "$curr"
-  echo "$f"
-  temp=`codegen "$f"|tail -1`
-  cd $temp && mvn -B compile 
+  codegenc "$f"
 done
