62f8690d4d2014a5a9211bbaa985d1888381b0b8
Dave Moten <davidmoten@gmail.com>
2024-05-06 13:56:34 +1000

    add summary to analyse.sh

--- openapi-codegen-generator/analyse.sh
+++ openapi-codegen-generator/analyse.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./analyse-base.sh 2&>~/oc-$(date -d "today" +"%Y%m%d%H%M").log
+TIMESTAMP=`date -d "today" +"%Y%m%d%H%M"`
+LOG="~/oc-$TIMESTAMP.log"
+echo writing logs to $LOG
+./analyse-base.sh 2&>$LOG
+grep -e "^codegen" -e "ERROR.*java" -e "Caused by" $LOG|grep "^codegen" -A 1|grep -v "^--"|grep -v "^codegen" -B 1|grep -v "^--" &>~/oc-$TIMESTAMP-errors.log
