fbe3218b7625899771d4bdb63ecba22c874f3cf2
Dave Moten <davidmoten@gmail.com>
2025-06-24 12:08:50 +1000

    javadoc addition

--- openapi-codegen-maven-plugin/src/main/java/org/davidmoten/oa3/codegen/plugin/GenerateMojo.java
+++ openapi-codegen-maven-plugin/src/main/java/org/davidmoten/oa3/codegen/plugin/GenerateMojo.java
@@ -56,10 +56,10 @@ public final class GenerateMojo extends AbstractMojo {
     private boolean failOnParseErrors;
 
     /**
-     * Allowed values: {@code springboot2}, {@code springboot3}, {@code spring2},
-     * {@code spring3}. {@code spring2} and {@code spring3} are deprecated and are
-     * aliases for {@code springboot2}, {@code springboot3}. Default is
-     * {@code springBoot2}.
+     * Allowed values (case-insensitive): {@code springboot2}, {@code springboot3},
+     * {@code spring2}, {@code spring3}. {@code spring2} and {@code spring3} are
+     * deprecated and are aliases for {@code springboot2}, {@code springboot3}.
+     * Default is {@code springBoot2}.
      */
     @Parameter(name = "generator", defaultValue = "springBoot2")
     private String generator;
